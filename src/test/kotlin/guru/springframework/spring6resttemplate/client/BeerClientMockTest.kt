f46703b2fca7282857f6272250b3468f2a7cb4f8
Евгений Галимсаров <address@email.com>
2023-05-24 17:02:24 +0500

    Mock OAuth2 Manager

--- src/test/kotlin/guru/springframework/spring6resttemplate/client/BeerClientMockTest.kt
+++ src/test/kotlin/guru/springframework/spring6resttemplate/client/BeerClientMockTest.kt
@@ -1,6 +1,7 @@
 package guru.springframework.spring6resttemplate.client
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import guru.springframework.spring6resttemplate.config.OAuthClientInterceptor
 import guru.springframework.spring6resttemplate.config.RestTemplateBuilderConfig
 import guru.springframework.spring6resttemplate.model.BeerDTO
 import guru.springframework.spring6resttemplate.model.BeerDTOtPageImpl
@@ -9,16 +10,29 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.mockito.ArgumentMatchers.any
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.client.RestClientTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.boot.test.web.client.MockServerRestTemplateCustomizer
 import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Import
 import org.springframework.data.domain.Page
 import org.springframework.http.HttpMethod
 import org.springframework.http.MediaType
+import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService
+import org.springframework.security.oauth2.client.registration.ClientRegistration
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository
+import org.springframework.security.oauth2.core.AuthorizationGrantType
+import org.springframework.security.oauth2.core.OAuth2AccessToken
 import org.springframework.test.web.client.MockRestServiceServer
 import org.springframework.test.web.client.match.MockRestRequestMatchers.*
 import org.springframework.test.web.client.response.MockRestResponseCreators.*
@@ -27,12 +41,14 @@ import org.springframework.web.client.RestTemplate
 import org.springframework.web.util.UriComponentsBuilder
 import java.math.BigDecimal
 import java.net.URI
+import java.time.Instant
 import java.util.*
 
 @Import(RestTemplateBuilderConfig::class)
 @RestClientTest
 class BeerClientMockTest {
     private val url = "http://localhost:8080"
+    private val bearerTest = "Bearer test"
 
     @Autowired
     private lateinit var restTemplateBuilderConfigured: RestTemplateBuilder
@@ -40,8 +56,43 @@ class BeerClientMockTest {
     @Autowired
     private lateinit var objectMapper: ObjectMapper
 
+    @Autowired
+    private lateinit var clientRegistrationRepository: ClientRegistrationRepository
+
     @Mock
     val mockRestTemplateBuilder: RestTemplateBuilder = RestTemplateBuilder(MockServerRestTemplateCustomizer())
+
+    @TestConfiguration
+    class TestConfig {
+        @Bean
+        fun clientRegistrationRepository(): ClientRegistrationRepository {
+            return InMemoryClientRegistrationRepository(
+                ClientRegistration
+                    .withRegistrationId("springauth")
+                    .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                    .clientId("test")
+                    .tokenUri("test")
+                    .build()
+            )
+        }
+
+        @Bean
+        fun auth2AuthorizedClientService(
+            clientRegistrationRepository: ClientRegistrationRepository
+        ): OAuth2AuthorizedClientService {
+            return InMemoryOAuth2AuthorizedClientService(clientRegistrationRepository)
+        }
+
+        @Bean
+        fun oAuthClientInterceptor(
+            manager: OAuth2AuthorizedClientManager, clientRegistrationRepository: ClientRegistrationRepository
+        ): OAuthClientInterceptor {
+            return OAuthClientInterceptor(manager, clientRegistrationRepository)
+        }
+    }
+
+    @MockBean
+    private lateinit var manager: OAuth2AuthorizedClientManager
 
     private lateinit var beerClient: BeerClient
 
@@ -52,6 +103,14 @@ class BeerClientMockTest {
 
     @BeforeEach
     fun setUp() {
+        val clientRegistration: ClientRegistration = clientRegistrationRepository.findByRegistrationId("springauth")
+
+        val token = OAuth2AccessToken(OAuth2AccessToken.TokenType.BEARER, "test", Instant.MIN, Instant.MAX)
+
+        Mockito
+            .`when`(manager.authorize(any()))
+            .thenReturn(OAuth2AuthorizedClient(clientRegistration, "test", token))
+
         val restTemplate: RestTemplate = restTemplateBuilderConfigured.build()
         server = MockRestServiceServer.bindTo(restTemplate).build()
         Mockito.`when`(mockRestTemplateBuilder.build()).thenReturn(restTemplate)
@@ -144,7 +203,7 @@ class BeerClientMockTest {
 
         server.expect(method(HttpMethod.GET))
             .andExpect(requestTo(uri))
-            .andExpect(header("Authorization", "Basic dXNlcjE6cGFzc3dvcmQ="))
+            .andExpect(header("Authorization", bearerTest))
             .andExpect(queryParam("beerName", "ALE"))
             .andExpect(queryParam("beerStyle", ""))
             .andExpect(queryParam("showInventory", "true"))
